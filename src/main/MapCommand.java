a20dd67c7ebd59a97e915aec0dfb72181fc94379
FilipeRL <filipe.rodrigueslourenco@gmail.com>
2025-05-26 11:10:33 +0200

    VERSION IFOUT les 2 truc en trop ( iprint et Array)

--- src/main/MapCommand.java
+++ src/main/MapCommand.java
@@ -1,10 +1,10 @@
-import utils.Array2DPrinter;
+import utils.Array2Dprinter;
  
 public class MapCommand implements ICommand {
     public String getKeyword() { return "map"; }
     public String getHelpText() { return "Affiche la carte du monde."; }
  
     public void execute(String args, Game game) {
-        Array2DPrinter.print2DArray(game.getWorldMap().getGrid());
+        Array2Dprinter.print2DArray(game.getWorldMap().getGrid());
     }
 }
